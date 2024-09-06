86c69090cca0c9d57fb22f0eb7cfec4823f77c3c
notcostheta <kaustubhbhiwsankar@gmail.com>
2024-09-06 19:30:26 +0000

    looks like half the work is done lmao, now need to make a webpage, fire up a server make it auto upload the image on some imageboard or smth fairly easy task to be honest

---
+++ sample_request.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+from os import getenv
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv("config.env")
+
+# Get API Key from environment variable OPENROUTER_API_KEY
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=getenv("OPENROUTER_API_KEY"),
+)
+
+
+# Create a completion request
+completion = client.chat.completions.create(
+    model="google/gemini-flash-8b-1.5-exp",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What is the image about?"},
+                {
+                    "type": "image_url",
+                    "image_url": {"url": "https://i.imgur.com/Yi3hL5c.jpeg"},
+                },
+            ],
+        },
+    ],
+)
+
+#
+# print(completion)
+
+# Save the entire response
+with open("response.json", "w") as f:
+    f.write(completion.json())
+    print("Response saved to response.json")
